929cb3d78d276b3c630f77b022900bbe45160364
jandadav <janda.david@gmail.com>
2020-04-10 13:28:50 +0200

    handle command submitting

--- src/components/Commandline/Commandline.js
+++ src/components/Commandline/Commandline.js
@@ -1,14 +1,31 @@
 import React,{ useState } from 'react';
 import { Card, Container, Row, Col, Button, Form } from 'react-bootstrap';
+const axios = require('axios').default;
 
 function Commandline() {
-
+    const IOT_ADDRESS = "http://mcu.local";
     const [command, setCommand] = useState("");
 
     function handleSubmit(event) {
         console.log("submit was handled");
         console.log(command); 
-        setCommand("");       
+        
+        
+        let formData = new FormData();
+        if(command.length>0){
+            formData.set("command", command)
+        }
+        axios({
+            method: 'post',
+            url: IOT_ADDRESS+'/post',
+            data: formData,
+            headers: {'Content-Type': 'multipart/form-data' }
+            })
+            .then( res => console.log(res))
+            .catch(function (error) {
+                console.log(error);
+            });
+        setCommand("");
     }
 
     //generic handler that can intercept and set state this.setState({form: {...this.state.form, [fieldName]: fleldVal}})
